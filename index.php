32e7b7a4c7715da02768981bbf3a1ed78800ef01
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2021-12-06 19:48:27 -0300

    tela inicial e login
    arquivos do bootstrap e tela de login e inicial

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tererê com Sociologia | Início</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css.map">
+</head>
+
+<body>
+    <header>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="navbar navbar-light">
+                    <div class="container-fluid">
+                        <div class="col-lg-4">
+                        <a class="titulo-navbar-verde">Tererê <span class="titulo-navbar-vermelho">com</span><br> Sociologia</a>
+                        </div>
+                        <div class="col-lg-8">
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="descubra algo incrível..." aria-label="Search">
+                            <!-- <button class="btn btn-outline-success" type="submit"></button> -->
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <hr>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    <script src="javascript/bootstrap.bundle.min.js"></script>
+    <script src="javascript/bootstrap.bundle.min.js.map"></script>
+</body>
+
+</html>
