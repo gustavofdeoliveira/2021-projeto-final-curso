d4f4cda9ee2ec68e13a6990714f983412f27d53d
Natan Gabriel Pastore Martins <55237805+NatanPastore@users.noreply.github.com>
2022-01-26 21:19:56 -0300

    Títulos corrigidos

--- view/Biblioteca.php
+++ view/Biblioteca.php
@@ -10,7 +10,7 @@ error_reporting(E_ERROR | E_PARSE);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tereré com Sociologia | Dashboard</title>
+    <title>Biblioteca | Tereré com Sociologia</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/responsive-theme.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
